46f9a7e5fef96387abc62e397ef008c183a9589b
Aurélien <aureliendrey@gmail.com>
2018-10-07 15:36:31 +0200

    Création de fake data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller {
+
+	public $danseurs=array(
+		array("id"=>0, "name"=>"Aurélien", "club"=>"DanseMulhouse", "date_naissance"=>"03/01/1999"),
+		array("id"=>1, "name"=>"Antoine", "club"=>"DanseAlsace", "date_naissance"=>"04/01/1999"),
+		array("id"=>2, "name"=>"Gauvain", "club"=>"DanseBretagne", "date_naissance"=>"05/01/1999"),
+		array("id"=>3, "name"=>"Mattéo", "club"=>"DanseBelfort", "date_naissance"=>"06/01/1999"),
+		array("id"=>4, "name"=>"Olivier", "club"=>"DanseLuxeuil", "date_naissance"=>"07/01/1999"),
+		array("id"=>5, "name"=>"Raphael", "club"=>"DanseVesoul", "date_naissance"=>"08/01/1999")
+	);
+
 	/**
 	 * @Route("/", name="home")
 	 * @param Request $request

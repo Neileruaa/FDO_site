6693e0b6cdc2a67c7cf846423c17cd56c6e611ec
olivier <salfran.olivier@gmail.com>
2018-11-10 13:44:35 +0100

    Ajout de la table Ticket

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Enitity\Club")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $NomClub;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $objet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $texte;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomClub(): ?string
+    {
+        return $this->NomClub;
+    }
+
+    public function setNomClub(string $NomClub): self
+    {
+        $this->NomClub = $NomClub;
+
+        return $this;
+    }
+
+    public function getObjet(): ?string
+    {
+        return $this->objet;
+    }
+
+    public function setObjet(string $objet): self
+    {
+        $this->objet = $objet;
+
+        return $this;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+}

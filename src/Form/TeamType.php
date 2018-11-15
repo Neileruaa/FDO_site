88e0b58ccd4838273f31b7a37161b87db09c1d81
aheitzm2 <antoinedebourb@gmail.com>
2018-11-15 08:43:57 +0100

    Modal ajouter Team

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class TeamType extends AbstractType
 {
@@ -18,6 +19,9 @@ class TeamType extends AbstractType
     {
         $builder
             ->add('dances', EntityType::class, array(
+                'constraints'=>new IsTrue(array('message'=>'Une équipe doit au moins être reliée à une danse')),
+                'label'=>'Liste des danses',
+                'attr' => ['class' => 'form-check'],
                 // query choices from this entity
                 'class' => Dance::class,
 
@@ -34,18 +38,23 @@ class TeamType extends AbstractType
             ))
 //            ->add('dances')
             ->add('dancers', EntityType::class, [
+                'constraints'=>new IsTrue(array('message'=>'Une équipe doit au moins être composé d\'un danseur')),
+                'label'=>'Liste des danseurs',
             	'class'=> Dancer::class,
 	            'choice_label'=>function(Dancer $dancer){
             	    return strval($dancer->getNameDancer()." ". $dancer->getFirstNameDancer());
 	            },
+                'required'=>true,
 	            'expanded'=>true,
 	            'multiple'=>true,
 	            'by_reference'=>false
             ])
 //            ->add('club')
 //            ->add('competitions')
-	        ->add('save', SubmitType::class)
-        ;
+	        ->add('save', SubmitType::class, [
+	            'label'=>'Ajouter l\'équipe',
+                'attr' => ['class' => 'btn btn-outline-success']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

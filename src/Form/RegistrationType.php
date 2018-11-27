08b35ec4dcfc1272e3f5d610ca78de354bb2c649
aheitzm2 <antoinedebourb@gmail.com>
2018-11-27 12:39:20 +0100

    traduction inscription

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -18,28 +18,36 @@ class RegistrationType extends AbstractType
     {
         $builder
             ->add('username',TextType::class, array(
-                'label'=>'Nom du club'
+                'label'=>'Nom du club',
+                'placeholder'=>'entrez le nom du club'
             ))
             ->add('nameClubOwner',TextType::class, array(
-                'label'=>'Nom du propriétaire du club'
+                'label'=>'Nom du gérant',
+                'placeholder'=>'entrez le nom du gerant du club'
             ))
             ->add('villeClub',TextType::class, array(
-                'label'=>"Ville"
+                'label'=>"Ville",
+                'placeholder'=>'entrez votre ville'
             ))
             ->add('codePostalClub',TextType::class, array(
-                'label'=>'Code postal'
+                'label'=>'Code postal',
+                'placeholder'=>'entrez le codePostal de votre ville'
             ))
             ->add('password',PasswordType::class, array(
-                'label'=>'Mot de passe'
+                'label'=>'Mot de passe',
+                'placeholder'=>'mot de passe'
             ))
             ->add('confirmPassword',PasswordType::class, array(
-                'label'=>'Confirmer le mot de passe'
+                'label'=>'Confirmer le mot de passe',
+                'placeholder'=>'confirmer votre mot de passe'
             ))
             ->add('emailClub', EmailType::class, array(
-                'label'=>'Adresse e-mail'
+                'label'=>'Adresse e-mail',
+                'placeholder'=>'entrez votre mail'
             ))
             ->add('phoneClub',TextType::class, array(
-                'label'=>'Numéro de téléphone'
+                'label'=>'Numéro de téléphone',
+                'placeholder'=>'entrez votre numero de telephone'
             ))
 
         ;

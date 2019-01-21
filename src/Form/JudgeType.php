241b5cf80c778041bc3bf2e76f5066bbaa44b153
Antoine <antoine.heitzmann2@gmail.com>
2019-01-21 18:45:25 +0100

    Onglet de gestion des juges

--- src/Form/JudgeType.php
+++ src/Form/JudgeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Judge;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +19,10 @@ class JudgeType extends AbstractType
             ])
             ->add('firstNameJudge', TextType::class,[
                 'label'=>'Prénom'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => ['class' => 'btn btn-outline-success'],
             ])
         ;
     }

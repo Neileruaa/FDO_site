2872ca724c771f5c9b7bc32c7ec07947cfb43b63
Antoine <antoine.heitzmann2@gmail.com>
2019-01-25 13:14:37 +0100

    Judge

---
+++ src/Controller/JudgeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Judge;
+use App\Form\JudgeType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class JudgeController extends AbstractController
+{
+    /**
+     * @Route("/judge/show", name="Judge.show")
+     */
+    public function show(Request $request, ObjectManager $manager)
+    {
+        $judges=$this->getDoctrine()->getRepository(Judge::class)->findAll();
+        $judge=new Judge();
+        $form = $this->createForm(JudgeType::class, $judge);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($judge);
+            $manager->flush();
+            return $this->redirectToRoute('Judge.show');
+        }
+        return $this->render('judge/show.html.twig', [
+            'judges' => $judges,
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/judge/delete/{id}", name="Judge.delete", requirements={"page"="\d+"})
+     * @param Judge $judge
+     * @isGranted("ROLE_ADMIN")
+     */
+    public function delete(Judge $judge, ObjectManager $manager){
+        $manager->remove($judge);
+        $manager->flush();
+        return $this->redirectToRoute('Judge.show');
+    }
+
+    /**
+     * @Route("/judge/edit/{id}", name="Judge.edit", requirements={"page"="\d+"})
+     * @isGranted("ROLE_ADMIN")
+     */
+    public function edit(Judge $judge, ObjectManager $manager, Request $request){
+        $form=$this->createForm(JudgeType::class, $judge);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($judge);
+            $manager->flush();
+            return $this->redirectToRoute('Judge.show');
+        }
+
+        return $this->render('judge/edit.html.twig', [
+            'form'=>$form->createView()
+        ]);
+    }
+}

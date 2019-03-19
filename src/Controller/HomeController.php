da6d14f65e001a11a9729930162d72294a47e053
Antoine <antoine.heitzmann2@gmail.com>
2019-03-19 13:45:37 +0100

    init dossards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,14 @@ class HomeController extends Controller {
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
     public function index(Request $request) {
+        $manager=$this->getDoctrine()->getManager();
+        $teams=$this->getDoctrine()->getRepository(Team::class)->findAll();
+        $cmpt=1;
+        foreach ($teams as $team){
+            $team->setNumDossard($cmpt);
+            $manager->persist($team);
+        }
+        $manager->flush();
         return $this->render(
         	'home/index.html.twig',
 	        array()
